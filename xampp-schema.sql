ffcfe165080e4dee2605490cdbd1df4f5270d12d
yanong2005 <yanong2005@users.noreply.github.com>
2026-09-05 21:48:12 +0800

    modified:   xampp-schema.sql

--- xampp-schema.sql
+++ xampp-schema.sql
@@ -1,11 +1,7 @@
 -- ------------------------------------------------------------
 -- Create Database
 -- ------------------------------------------------------------
-CREATE DATABASE IF NOT EXISTS student_monitoring
-    CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
 
-USE student_monitoring;
 
 -- ------------------------------------------------------------
 -- 1. Users (authentication & roles)
